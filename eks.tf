b76fe49c9adee919808409efbc1c82a6333bb622
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-23 18:39:40 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,7 +1,7 @@
 module "eks_cluster" {
   source                = "terraform-aws-modules/eks/aws"
   cluster_name          = "mohan_eks"
-  cluster_version       = "1.21"
+  cluster_version       = "1.25"
   vpc_id                = aws_vpc.main.id
   subnet_ids            = [aws_subnet.private1.id, aws_subnet.private2.id]
   eks_managed_node_group_defaults = {
