41ae0605c09c5ff20151a41210143dd2bb0ef4b7
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-23 18:34:09 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -4,12 +4,18 @@ module "eks_cluster" {
   cluster_version       = "1.21"
   vpc_id                = aws_vpc.main.id
   subnet_ids            = [aws_subnet.private1.id, aws_subnet.private2.id]
-  node_groups           = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t3.medium"
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    instance_types         = ["t3.medium"]
+    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  }
+
+  eks_managed_node_groups = {
+
+    node_group = {
+      min_size     = 2
+      max_size     = 6
+      desired_size = 2
     }
   }
 }
