43ed46e4579569df6c35135687428c1d639a03eb
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-06-04 19:05:16 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,12 +1,22 @@
-module "eks_cluster" {
-  source                = "terraform-aws-modules/eks/aws"
-  cluster_name          = "mohan_eks"
-  cluster_version       = "1.25"
-  vpc_id                = aws_vpc.main.id
-  subnet_ids            = [aws_subnet.private1.id, aws_subnet.private2.id]
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
+  cluster_name    = local.cluster_name
+  cluster_version = var.kubernetes_version
+  subnet_ids      = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  tags = {
+    cluster = "demo"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
   eks_managed_node_group_defaults = {
     ami_type               = "AL2_x86_64"
     instance_types         = ["t3.medium"]
+    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
   }
 
   eks_managed_node_groups = {
