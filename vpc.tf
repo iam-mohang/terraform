f84fcbad4609534b72909aefaf4019aaa6bc6cff
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-06-04 19:26:25 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,36 +1,43 @@
-
-resource "aws_security_group" "all_worker_mgmt" {
-  name_prefix = "all_worker_management"
-  vpc_id      = module.vpc.vpc_id
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
-  description       = "allow inbound traffic from eks"
-  from_port         = 443
-  protocol          = "-1"
-  to_port           = 443
-  security_group_id = aws_security_group.all_worker_mgmt.id
-  type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "project-eks-${random_string.suffix.result}"
 }
 
-resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
-  description       = "allow inbound traffic from eks"
-  from_port         = 80
-  protocol          = "-1"
-  to_port           = 80
-  security_group_id = aws_security_group.all_worker_mgmt.id
-  type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.7.0"
 
-resource "aws_security_group_rule" "all_worker_mgmt_egress" {
-  description       = "allow outbound traffic to anywhere"
-  from_port         = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.all_worker_mgmt.id
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
+  name                 = "project-eks-vpc"
+  cidr                 = var.vpc_cidr
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
 }
