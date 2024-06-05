79c4f3f2aa081694d464ea7df6856371643441d1
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-06-04 19:23:18 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,56 +1,36 @@
-provider "aws" {
-  region = "us-east-1"
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-}
-
-resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_subnet" "private2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
-}
-
-resource "aws_security_group_rule" "eks_ingress_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]  # Allow traffic from any source
-}
-
-resource "aws_security_group_rule" "eks_ingress_https" {
-  type              = "ingress"
+resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
+  description       = "allow inbound traffic from eks"
   from_port         = 443
+  protocol          = "-1"
   to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]  # Allow traffic from any source
+  security_group_id = aws_security_group.all_worker_mgmt.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "eks_egress_all" {
-  type              = "egress"
+resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
+  description       = "allow inbound traffic from eks"
+  from_port         = 80
+  protocol          = "-1"
+  to_port           = 80
+  security_group_id = aws_security_group.all_worker_mgmt.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+
+resource "aws_security_group_rule" "all_worker_mgmt_egress" {
+  description       = "allow outbound traffic to anywhere"
   from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.all_worker_mgmt.id
   to_port           = 0
-  protocol          = "-1"  # Allow all protocols
-  cidr_blocks       = ["0.0.0.0/0"]  # Allow traffic to any destination
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
-
